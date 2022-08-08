f9e9da7d368dfd37247eb85bbe8d1e94309fd6d0
taku820801 <takuchanpower@gmail.com>
2022-08-08 23:16:42 +0900

    クラス追加

--- src/js/stage.ts
+++ src/js/stage.ts
@@ -4,6 +4,7 @@ class Stage {
     static scoreElement: HTMLElement;
     static board: number[][];
     static yasaiCount: number;
+    static fallingYasaiList: [];
 
     static initialize() {
         //htmlからステージの元となる要素を取得し大きさを設定する
@@ -58,8 +59,80 @@ class Stage {
         }
         this.yasaiCount = yasaiCount;
     }
-    //TypeScript追加
+
+    //画面とメモリ両方にyasaiをセットする
     static setYasai(x: number, y: number, yasai: number) {
-        throw new Error("Method not implemented.");
+        //画像を作成し配置する
+        const yasaiImage = YasaiImage.getYasai(yasai);
+        yasaiImage.style.left = x * Config.yasaiImgWidth + 'px';
+        yasaiImage.style.top = y * Config.yasaiImgHeigth + 'px';
+        this.stageElement.appendChild(yasaiImage);
+        //メモリにセットする
+        this.board[y][x] = {
+            yasai: yasai,
+            element: yasaiImage
+        }
+    }
+
+    //自由落下をチェックする
+    static cheackFall() {
+        this.fallingYasaiList.length = 0;
+        let isFalling = false;
+        //下の行から上の行を見ていく
+        for(let y = Config.stageRows - 2; y >= 0; y--) {
+            const line = this.board[y];
+            for(let x = 0; x < line.length; x++) {
+                if(!this.board[x][y]) {
+                    //このマスに野菜がなければ次
+                    continue;
+                }
+                if(!this.board[y + 1][x]) {
+                    //この野菜は落ちるので取り除く
+                    let cell = this.board[y][x];
+                    this.board[y][x] = 0;
+                    let dst = y;
+                    while(dst + 1 < Config.stageRows && this.board[dst + 1][x] == null) {
+                        dst++;
+                    }
+                    //最終目的地に置く
+                    this.board[dst][x] = cell;
+                    //落ちるリストに入れる
+                    this.fallingYasaiList.push({
+                        element: cell.element,
+                        position: y * Config.yasaiImgHeigth,
+                        destination: dst * Config.yasaiImgHeigth,
+                        falling: true,
+                    });
+                    //落ちるものがあった事を記憶しておく
+                    isFalling = true;
+                }
+            }
+        }
+        return isFalling;
+    }
+    //自由落下させる
+    static fall() {
+        let isFalling = false;
+        for(const fallingYasai of this.fallingYasaiList) {
+            if(!fallingYasai.falling) {
+                //自由落下が終わっている
+                continue;
+            }
+            let position = fallingYasai.position;
+            position += Config.freeFallingSpeed;
+            if(position >= fallingYasai.destination) {
+                //自由落下終わり
+                position = fallingYasai.destination;
+                fallingYasai.falling = false;
+            } else {
+                //まだ落下している野菜がある事を記憶する
+                isFalling = true;
+            }
+            //新しい位置を保存する
+            fallingYasai.position = position;
+            //野菜を動かず
+            fallingYasai.element.style.top = position + 'px';
+        }
+        return isFalling;
     }
 }
